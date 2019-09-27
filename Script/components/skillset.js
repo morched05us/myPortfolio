a71349ea8e143198e98948d39ca4980c070922e3
Morchid Fsdg <morchid000@gmail.com>
2019-09-27 19:23:03 +0100

    first

--- Script/components/skillset.js
+++ Script/components/skillset.js
@@ -1,28 +1,93 @@
-document.getElementById('frontendFlip').addEventListener('click', function(){
-    document.getElementById('c-skillset__frontend').style = "transform: rotateY(180deg)";
-    document.getElementById('c-skillset__backend').style = "transform: rotateY(0deg)";
-    document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
+var front = true;
+var back = true;
+var skills = true;
+
+
+document.getElementById('c-skillset__frontend').addEventListener('click', function () {
+    if (front) {
+        document.getElementById('c-skillset__frontend').style = "transform: rotateY(180deg)";
+        document.getElementById('c-skillset__backend').style = "transform: rotateY(0deg)";
+        document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
+        setTimeout(() => {
+            front = false;
+            back = true;
+            skills = true;
+        }, 10);
+    };
 });
 
-document.getElementById('backendFlip').addEventListener('click', function(){
-    document.getElementById('c-skillset__backend').style = "transform: rotateY(180deg)";
+document.getElementById('c-skillset__frontend__back').addEventListener('click', function () {
     document.getElementById('c-skillset__frontend').style = "transform: rotateY(0deg)";
-    document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
+    setTimeout(() => {
+        skills = true;
+        front = true;
+        back = true;
+    }, 10);
 });
 
-document.getElementById('skillsFlip').addEventListener('click', function(){
+document.getElementById('c-skillset__backend').addEventListener('click', function () {
+    if (back) {
+        document.getElementById('c-skillset__backend').style = "transform: rotateY(180deg)";
+        document.getElementById('c-skillset__frontend').style = "transform: rotateY(0deg)";
+        document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
+        setTimeout(() => {
+            back = false;
+            front = true;
+            skills = true;
+        }, 10);
+    }
+});
+
+document.getElementById('c-skillset__backend__back').addEventListener('click', function () {
     document.getElementById('c-skillset__backend').style = "transform: rotateY(0deg)";
-    document.getElementById('c-skillset__frontend').style = "transform: rotateY(0deg)";
-    document.getElementById('c-skillset__skills').style = "transform: rotateY(180deg)";
+    setTimeout(() => {
+        skills = true;
+        front = true;
+        back = true;
+    }, 10);
 });
 
-document.addEventListener('click', function(event) {
+document.getElementById('c-skillset__skills').addEventListener('click', function () {
+    if (skills) {
+        document.getElementById('c-skillset__backend').style = "transform: rotateY(0deg)";
+        document.getElementById('c-skillset__frontend').style = "transform: rotateY(0deg)";
+        document.getElementById('c-skillset__skills').style = "transform: rotateY(180deg)";
+        setTimeout(() => {
+            skills = false;
+            front = true;
+            back = true;
+        }, 10);
+    }
+});
+
+document.getElementById('c-skillset__skills__back').addEventListener('click', function () {
+    document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
+    setTimeout(() => {
+        skills = true;
+        front = true;
+        back = true;
+    }, 10);
+});
+
+
+document.addEventListener('click', function (event) {
     var isClickInside = document.getElementById('c-skillset').contains(event.target);
-  
+
     if (!isClickInside) {
         document.getElementById('c-skillset__frontend').style = "transform: rotateY(0deg)";
         document.getElementById('c-skillset__backend').style = "transform: rotateY(0deg)";
         document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
     }
+});
+
+
+window.addEventListener('scroll', () => {
+    document.getElementById('c-skillset__frontend').style = "transform: rotateY(0deg)";
+    document.getElementById('c-skillset__backend').style = "transform: rotateY(0deg)";
+    document.getElementById('c-skillset__skills').style = "transform: rotateY(0deg)";
+    setTimeout(() => {
+        skills = true;
+        front = true;
+        back = true;
+    }, 10);
   });
-  
